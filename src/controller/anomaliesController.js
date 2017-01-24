2b9f49b2628cef555c60daebc5ea4cca6ba47e3a
dimhold <dimhold@gmail.com>
2017-01-24 18:25:14 +0300

    Implement controls buttons for anomalies

--- src/controller/anomaliesController.js
+++ src/controller/anomaliesController.js
@@ -1,6 +1,14 @@
 var randoApp = angular.module("randoApp");
 
 randoApp.controller("AnomaliesController", function($scope, $http, $routeParams) {
+  $scope.refreshAnomalies = function (anomalies) {
+    for (var i = 0; i < anomalies.length; i++) {
+      anomalies[i].detectedAtPritty = moment.utc(anomalies[i].detectedAt).format('DD MMMM YYYY, HH:mm:ss');
+    }
+    $scope.anomalies = anomalies;
+    $scope.allAnomalies = anomalies;
+  };
+
   $scope.countByTag = function (anomalies, tag) {
     return anomalies.filter( (anomaly) => {
       return anomaly.discrepancyReason === tag;
@@ -13,13 +21,30 @@ randoApp.controller("AnomaliesController", function($scope, $http, $routeParams)
     });
   };
 
-  $http.get("/anomalies").success(function (anomalies) {
-    for (var i = 0; i < anomalies.length; i++) {
-      anomalies[i].detectedAtPritty = moment.utc(anomalies[i].detectedAt).format('DD MMMM YYYY, HH:mm:ss');
+  $scope.moveToRandosBucket = function (randoId) {
+    if (confirm("Your rando will be moved to randos bucket. All tags will be removed. Are you sure?")) {
+      $http.post("/anomaly-move/" + randoId).success(function (anomalies) {
+        $scope.refreshAnomalies(anomalies);
+        alert("Anomaly MOVED. RandoId: " + randoId);
+      }).error(function (err) {
+        alert("CanNOT move anomaly with RandoId: " + randoId + " because: " + JSON.stringify(err));
+      });
     }
-    $scope.anomalies = anomalies;
-    $scope.allAnomalies = anomalies;
+  };
+
+  $scope.deleteFromAnomalies = function (randoId) {
+    if (confirm("Your rando will be DELETE from anomalies, but will be save in user out. Are you sure that you want to delete this anomaly?")) {
+      $http.post("/anomaly-delete/" + randoId).success(function (anomalies) {
+        $scope.refreshAnomalies(anomalies);
+        alert("Anomaly DELETED. RandoId: " + randoId);
+      }).error(function (err) {
+        alert("CanNOT delete anomaly with RandoId: " + randoId + " because: " + JSON.stringify(err));
+      });
+    }
+  };
+
+  $http.get("/anomalies").success(function (anomalies) {
+    $scope.refreshAnomalies(anomalies);
   });
-
+  
 });
-
