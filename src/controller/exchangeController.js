ad988d04ecc1a0bab87e4e7da70510ba8bf41bad
dimhold <dimhold@gmail.com>
2017-02-07 12:04:00 +0300

    Add next and back buttons logic

--- src/controller/exchangeController.js
+++ src/controller/exchangeController.js
@@ -1,6 +1,21 @@
 var randoApp = angular.module("randoApp");
 
 randoApp.controller("ExchangeController", function($scope, $http, $routeParams) {
+
+  $scope.nextExchange = function () {
+    $scope.exchangeIndex++;
+    if ($scope.exchangeIndex >= $scope.exchange.length ) {
+      $scope.exchangeIndex = $scope.exchangeIndex.length - 1;
+    }
+  };
+
+  $scope.prevExchange = function () {
+    $scope.exchangeIndex--;
+    if ($scope.exchangeIndex <= 0) {
+      $scope.exchangeIndex = 0;
+    }
+  };
+
   $http.get("/exchangelogs?limit=50").success(function (exchange) {
     $scope.exchange = exchange;
     $scope.exchangeIndex = 0;
