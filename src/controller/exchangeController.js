1a158c61080e5eedbd44a0231ab48e74339f5217
dimhold <dimhold@gmail.com>
2017-02-07 12:39:14 +0300

    Implement table

--- src/controller/exchangeController.js
+++ src/controller/exchangeController.js
@@ -24,6 +24,20 @@ randoApp.controller("ExchangeController", function($scope, $http, $location) {
     }
   };
 
+  $scope.getAllMetrics = function (exchange) {
+    return Array.from(new Set(exchange.metics.map(metrics => metrics.metrica))).sort();
+  };
+
+  $scope.getRandosIds = function (exchange) {
+    return Array.from(new Set(exchange.metics.map(metrics => metrics.randoId))).sort();
+  };
+
+  $scope.getMarksForRandoId = function (randoId) {
+    var marks = exchange.metics.filter(metrics => {{return randoId === metrics.randoId)));
+    marks.sort((a,b) => {return (a.metrica > b.metrica) ? 1 : ((b.metrica > a.metrica) ? -1 : 0);}); 
+    return marks;
+  };
+
   $http.get("/exchangelogs?limit=50").success(function (exchange) {
     $scope.exchange = exchange;
     $scope.exchangeIndex = 0;
