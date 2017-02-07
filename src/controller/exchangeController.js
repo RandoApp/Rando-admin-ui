93773b8c5015eb114f54ead7c0c8177d786fa31b
dimhold <dimhold@gmail.com>
2017-02-07 11:58:58 +0300

    Add images for exchange

--- src/controller/exchangeController.js
+++ src/controller/exchangeController.js
@@ -3,6 +3,7 @@ var randoApp = angular.module("randoApp");
 randoApp.controller("ExchangeController", function($scope, $http, $routeParams) {
   $http.get("/exchangelogs?limit=50").success(function (exchange) {
     $scope.exchange = exchange;
+    $scope.exchangeIndex = 0;
   });
   
 });
