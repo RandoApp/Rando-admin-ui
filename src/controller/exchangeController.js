6cd3c3144ddb2170653884aabf210877999485d0
dimhold <dimhold@users.noreply.github.com>
2017-02-08 11:22:49 +0300

    Add Exchange tab (#6)
    * Add exchange log
    
    * Add exchange log
    
    * Add images for exchange
    
    * Add next and back buttons logic
    
    * Add next and back buttons logic
    
    * Add dev prefix
    
    * Implement table
    
    * Fix syntax error
    
    * Fit iterators
    
    * Update html
    
    * Fix typo
    
    * Fix table
    
    * Fix typo
    
    * Try add email to table
    
    * Try add email to table
    
    * Try add email to table
    
    * Try add email to table
    
    * Try add email to table
    
    * Try add email to table
    
    * Update html
    
    * Add css for randos
    
    * Add css for randos
    
    * Add exchangeAt
    
    * Add chosen
    
    * Update css
    
    * Update css
    
    * Update css
    
    * Update css
    
    * Update css
    
    * Update css
    
    * Update css
    
    * Fix ng-class
    
    * Fix ng-class
    
    * Update css
    
    * Fix next button
    
    * Update buttons

---
+++ src/controller/exchangeController.js
@@ -0,0 +1,58 @@
+var randoApp = angular.module("randoApp");
+
+randoApp.controller("ExchangeController", function($scope, $http, $location) {
+
+  $scope.nextExchange = function () {
+    $scope.exchangeIndex++;
+    if ($scope.exchangeIndex >= $scope.exchange.length ) {
+      $scope.exchangeIndex = $scope.exchange.length - 1;
+    }
+  };
+
+  $scope.prevExchange = function () {
+    $scope.exchangeIndex--;
+    if ($scope.exchangeIndex <= 0) {
+      $scope.exchangeIndex = 0;
+    }
+  };
+
+  $scope.getDevPrefix = function () {
+    if ($location.host().indexOf("dev") !== -1) {
+      return "dev.";
+    } else {
+      return "";
+    }
+  };
+
+  $scope.getAllMetrics = function (exchange) {
+    return Array.from(new Set(exchange.metrics.map(metrics => metrics.metrica))).sort();
+  };
+
+  $scope.getRandosIds = function (exchange) {
+    return Array.from(new Set(exchange.metrics.map(metrics => metrics.randoId))).sort();
+  };
+
+  $scope.getRandoTitleByRandoId = function (randoId, exchange) {
+    return randoId.substring(0, 6) + " - " + exchange.randos.filter(rando => {return randoId === rando.randoId})[0].email;
+  };
+
+  $scope.getMarksForRandoId = function (randoId, exchange) {
+    var marks = exchange.metrics.filter(metrics => {return randoId === metrics.randoId});
+    marks.sort((a,b) => {return (a.metrica > b.metrica) ? 1 : ((b.metrica > a.metrica) ? -1 : 0);}); 
+    return marks;
+  };
+
+  $scope.isRandoChooser = function (randoId, exchange) {
+    return exchange.choosers.filter(chooser => {return randoId === chooser.randoId}).length > 0;
+  };
+  
+  $scope.getPrettyDateTime = function (time) {
+    return moment.utc(time).format('DD MMMM YYYY, HH:mm:ss');
+  }
+
+  $http.get("/exchangelogs?limit=50").success(function (exchange) {
+    $scope.exchange = exchange;
+    $scope.exchangeIndex = 0;
+  });
+  
+});
